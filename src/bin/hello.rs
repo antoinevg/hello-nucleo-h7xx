1284160aa2915ad642381de3967a9221f09c9b4a
Antoine van Gelder <antoine@flowdsp.io>
2021-03-30 15:14:08 +0200

    Update sample code to reflect latest nucleo board support crate API's

--- src/bin/hello.rs
+++ src/bin/hello.rs
@@ -2,38 +2,54 @@
 #![no_std]
 
 use nucleo_h745zi as nucleo;
+use nucleo::hal::prelude::*;
 use nucleo::led::Led;
 
 
 #[cortex_m_rt::entry]
 fn main() -> ! {
-    defmt::info!("Hello, {{crate_name}}!");
+    defmt::info!("Hello, bar!");
 
     // - board setup ----------------------------------------------------------
 
-    let board = nucleo::Board::take().unwrap();
-    let user_leds = &mut board.user_leds;
+    let board = nucleo::board::Board::take().unwrap();
+
+    let dp = nucleo::pac::Peripherals::take().unwrap();
+
+    let ccdr = board.freeze_clocks(dp.PWR.constrain(),
+                                   dp.RCC.constrain(),
+                                   &dp.SYSCFG);
+
+    let pins = board.split_gpios(dp.GPIOA.split(ccdr.peripheral.GPIOA),
+                                 dp.GPIOB.split(ccdr.peripheral.GPIOB),
+                                 dp.GPIOC.split(ccdr.peripheral.GPIOC),
+                                 dp.GPIOD.split(ccdr.peripheral.GPIOD),
+                                 dp.GPIOE.split(ccdr.peripheral.GPIOE),
+                                 dp.GPIOF.split(ccdr.peripheral.GPIOF),
+                                 dp.GPIOG.split(ccdr.peripheral.GPIOG));
+
+    let mut user_leds = nucleo::led::UserLeds::new(pins.user_leds);
 
 
     // - main loop ------------------------------------------------------------
 
-    let one_second = board.clocks.sys_ck().0;
+    let one_second = ccdr.clocks.sys_ck().0;
 
     for n in 0..10 {
-        user_leds.LD3.off();
-        user_leds.LD1.on();
+        user_leds.ld3.off();
+        user_leds.ld1.on();
         cortex_m::asm::delay(one_second);
 
-        user_leds.LD1.off();
-        user_leds.LD2.on();
+        user_leds.ld1.off();
+        user_leds.ld2.on();
         cortex_m::asm::delay(one_second);
 
-        user_leds.LD2.off();
-        user_leds.LD3.on();
+        user_leds.ld2.off();
+        user_leds.ld3.on();
         cortex_m::asm::delay(one_second);
 
         defmt::info!("loop: {:?} of 10", n + 1);
     }
 
-    {{crate_name}}::exit()
+    bar::exit()
 }
